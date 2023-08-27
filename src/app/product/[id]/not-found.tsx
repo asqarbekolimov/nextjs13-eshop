c7159ed7047832d41a298887e035c0e6d371246c
asqarbek olimov <88034288+asqarbekolimov@users.noreply.github.com>
2023-08-27 10:51:50 +0500

    Prod detail

---
+++ src/app/product/[id]/not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="mt-24">
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,35 @@
+import CustomImage from "@/components/image";
+import { notFound } from "next/navigation";
 import React from "react";
 
-export default function ProductDetailPage() {
-  return <div>ProductDetailPage</div>;
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default async function ProductDetailPage({ params: { id } }: Props) {
+  try {
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+    const product = await res.json();
+    return (
+      <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-48 pb-10">
+        <CustomImage product={product} />
+
+        <div className="divide-x-2">
+          <div className="space-y-2 pb-8">
+            <h1 className="text-2xl md:text-4xl font-bold">{product.title}</h1>
+            <h2 className="text-gray-500 font-bold text-xl md:text-3xl">
+              ${product.price}
+            </h2>
+          </div>
+          <div className="pt-8">
+            <p className="text-xs md:text-sm">{product.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  } catch (error) {
+    notFound();
+  }
 }
